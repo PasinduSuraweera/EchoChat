ac1a1e213292c0a5484f6d2e5485631698cbc8c7
PasinduSuraweera <pasisuraweera@gmail.com>
2025-06-26 12:51:50 +0530

    sidebar change 2

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = () => {
         <Link to="/" className="flex items-center gap-2.5">
           <MessagesSquare className="size-9 text-primary" />
           <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-            EchoChatrr
+            EchoChat
           </span>
         </Link>
       </div>
