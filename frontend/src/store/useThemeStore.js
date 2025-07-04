3651d68d4942a5391bce3add65a19446c9c8b21c
Pasindu Suraweera <pasisuraweera2003@gmail.com>
2025-07-04 23:07:27 +0530

    loader fix

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("echo-theme") || "coffee",
+  theme: localStorage.getItem("echo-theme") || "lemonade",
   setTheme: (theme) => {
     localStorage.setItem("echo-theme", theme);
     set({ theme });
